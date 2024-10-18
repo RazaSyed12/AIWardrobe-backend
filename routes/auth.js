ebdd17316e3573d127300101c3864e7f8ef6806c
RazaSyed12 <106811532+RazaSyed12@users.noreply.github.com>
2024-10-19 01:36:20 +0500

    Removed unwanted code

--- routes/auth.js
+++ routes/auth.js
@@ -5,44 +5,6 @@ import User from "../models/User.js";
 
 const router = express.Router();
 
-// Register a new user
-// router.post("/register", async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-
-//     // Check if the username is already taken
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) {
-//       return res.status(400).json({ error: "Username already taken" });
-//     }
-
-//     // Create a new user
-//     const hashedPassword = await bcrypt.hash(password, 8);
-//     const user = new User({ username, password: hashedPassword });
-
-//     // Save the user to the database
-//     await user.save();
-
-//     // Generate a JWT token
-//     const token = jwt.sign(
-//       { _id: user._id.toString() },
-//       process.env.JWT_SECRET,
-//       {
-//         expiresIn: "1h", // Token expires in 1 hour
-//       }
-//     );
-
-//     res
-//       .status(201)
-//       .json({ message: "User registered successfully", user, token });
-//   } catch (error) {
-//     console.error("Registration error:", error); // Log the actual error message
-//     res
-//       .status(500)
-//       .json({ error: "Error registering user", details: error.message });
-//   }
-// });
-
 router.post("/register", async (req, res) => {
   try {
     const {
@@ -108,63 +70,6 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// Login a user
-// router.post("/login", async (req, res) => {
-//   try {
-//     const { usernameOrEmail, password } = req.body;
-
-//     // Find the user by username or email
-//     const user = await User.findOne({
-//       $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
-//     });
-
-//     if (!user) {
-//       console.log("User not found"); // Debugging line
-//       return res
-//         .status(400)
-//         .json({ error: "Invalid username/email or password" });
-//     }
-
-//     console.log("User found:", user); // Debugging line
-
-//     // Compare the provided password with the hashed password
-//     const isMatch = await bcrypt.compare(password, user.password);
-
-//     console.log("Password Match:", isMatch); // Debugging line
-
-//     if (!isMatch) {
-//       return res
-//         .status(400)
-//         .json({ error: "Invalid username/email or password" });
-//     }
-
-//     // Generate a JWT token
-//     const token = jwt.sign(
-//       { _id: user._id.toString(), role: user.role },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "1h" }
-//     );
-
-//     res.status(200).json({
-//       message: "Login successful",
-//       user: {
-//         _id: user._id,
-//         username: user.username,
-//         email: user.email,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         role: user.role,
-//       },
-//       token,
-//     });
-//   } catch (error) {
-//     console.error("Login error:", error.message);
-//     res
-//       .status(500)
-//       .json({ error: "Error logging in user", details: error.message });
-//   }
-// });
-
 router.post("/login", async (req, res) => {
   try {
     const { usernameOrEmail, password } = req.body;
@@ -175,20 +80,12 @@ router.post("/login", async (req, res) => {
     });
 
     if (!user) {
-      console.log("User not found"); // Debugging line
       return res
         .status(400)
         .json({ error: "Invalid username/email or password" });
     }
 
-    console.log("User found:", user); // Debugging line
-
-    // Compare the provided password with the hashed password
-    console.log("Provided Password:", password); // Debugging line
-    console.log("Stored Hashed Password:", user.password); // Debugging line
-
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log("Password Match:", isMatch); // Debugging line
 
     if (!isMatch) {
       return res
